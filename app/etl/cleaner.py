1877671ea4054056b100b1cc5801bdf35578b952
ChaosKingNV <namannaman.v32@gmail.com>
2024-12-08 21:02:56 -0500

    Splitting the ETL logic into different files and keep app.py clean and simple.

--- app/etl/cleaner.py
+++
@@ -1,7 +0,0 @@
-def clean_data(raw_data):
-    # Example of a basic cleaning function
-    cleaned_data = []
-    for item in raw_data:
-        # Remove unnecessary parts or clean up the text
-        cleaned_data.append(item)
-    return cleaned_data
